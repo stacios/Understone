dc735bc15e581cb2d98860fab7576c1f39fee9ac
Ares <areszee7@gmail.com>
2024-04-14 15:44:28 -0700

    Ares corrected his java file!

--- areszhello.java
+++ areszhello.java
@@ -1,5 +1,5 @@
-public class Hello{
-  public static void main(String [] args) {
+public class areszhello {
+  public static void main(String[] args) {
     System.out.println("Hello from Ares!");
   }
 }
