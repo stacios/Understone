20a8e59f108f07c2357f6b0c5920163eaa2c3136
Ares <areszee7@gmail.com>
2024-05-12 23:48:44 -0700

    Create CharacterTypes.java
    Created Dwarf String Types

---
+++ src/Model/CharacterTypes.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * Static class DwarfTypes defining String Representations of Dwarf Types.
+ */
+public class CharacterTypes {
+    public static String DRILLER = "Driller";
+    public static String ENGINEER = "Engineer";
+    public static String GUNNER = "Gunner";
+    public static String SCOUT = "Scout";
+}
