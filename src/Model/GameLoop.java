8d5ab5f1d43646b5bb23791455ee0eab91ed8242
labanlab <labanlab@uw.edu>
2024-05-14 19:52:15 -0700

    fixed a typo in game loop

--- src/Model/GameLoop.java
+++ src/Model/GameLoop.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 
 import static Model.CharacterTypes.*;
 
-public class GameLoop {
 
 public class GameLoop implements Serializable {
     private static final long serialVersionUID = 8L;
