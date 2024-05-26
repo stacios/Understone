4e247459ef97ab21f53a782f2cb4e057f61bcca1
labanlab <labanlab@uw.edu>
2024-05-25 21:28:24 -0700

    upd test size

--- src/Model/HUD.java
+++ src/Model/HUD.java
@@ -13,7 +13,7 @@ public class HUD implements Drawable, Serializable {
     private static final long serialVersionUID = 1L;
     private final Dwarf myPlayer;
     private static final Map<String, List<String>> dwarfWeapons;
-    private static final int TEXT_HEIGHT = 20;
+    private static final int TEXT_HEIGHT = 30;
 
     static {
         dwarfWeapons = new HashMap<>();
@@ -33,30 +33,30 @@ public class HUD implements Drawable, Serializable {
 
         // Display Dwarf type
         int dwarfTextX = 20;
-        int dwarfTextY = Display.getInstance().getHeight() - 200; // Moved up by 30 pixels
+        int dwarfTextY = Display.getInstance().getHeight() - 250;
         String dwarfType = myPlayer.getName();
         drawData.add(getTextDrawData("Dwarf " + dwarfType, dwarfTextX, dwarfTextY));
 
         // Display health
-        int healthTextY = dwarfTextY + 30;
+        int healthTextY = dwarfTextY + 45;
         int health = myPlayer.getHealth();
         drawData.add(getTextDrawData("Health " + health, dwarfTextX, healthTextY));
 
         // Display weapon
-        int weaponTextY = healthTextY + 30;
+        int weaponTextY = healthTextY + 45;
         String myWeapon = getCurrentWeapon(dwarfType, myPlayer.getWeaponIndex());
         drawData.add(getTextDrawData("Weapon " + myWeapon, dwarfTextX, weaponTextY));
 
         // Display weapon image below the weapon text
         int weaponImageX = dwarfTextX + 100;
-        int weaponImageY = weaponTextY + 30; // Positioned below the weapon text
-        drawData.add(getImageDrawData(myWeapon, weaponImageX, weaponImageY, 70, 70));
+        int weaponImageY = weaponTextY + 45;
+        drawData.add(getImageDrawData(myWeapon, weaponImageX, weaponImageY, 80, 80));
 
         return drawData.toArray(new String[0]);
     }
 
     private String getTextDrawData(String text, int x, int y) {
-        return "text:" + text + ":" + x + ":" + y + ":20:20";
+        return "text:" + text + ":" + x + ":" + y + ":30:30";
     }
 
     private String getImageDrawData(String imageName, int x, int y, int width, int height) {
@@ -68,6 +68,7 @@ public class HUD implements Drawable, Serializable {
         return weapons != null && weaponIndex < weapons.size() ? weapons.get(weaponIndex) : "Unknown";
     }
 }
+
 
 
 
