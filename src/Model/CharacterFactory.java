60b8c3abf5a3bb4a601695de94125be6a6867ee5
Ares <areszee7@gmail.com>
2024-06-01 14:27:35 -0700

    Updated CharacterFactory with Rock and Heal types

--- src/Model/CharacterFactory.java
+++ src/Model/CharacterFactory.java
@@ -129,7 +129,7 @@ public class CharacterFactory {
     }
 
     public static Rock createObject(final String theRockType) {
-        if (!theRockType.equals(ROCK) && !theRockType.equals(EGG)) {
+        if (!theRockType.equals(HEAL) && !theRockType.equals(EGG) && !theRockType.equals(ROCK)) {
             throw new Error("Must be of type Egg or Rock");
         }
 
@@ -138,8 +138,10 @@ public class CharacterFactory {
         switch (theRockType) {
             case ROCK:
                 return new Rock(theRockType, 600, 500, 100, 200, 200, 0, defaultWeapon, 0, 0);
+            case HEAL:
+                return new Rock(theRockType, 600, 500, 50, 75, 75, 0, defaultWeapon, 0, 0);
             case EGG:
-                return new Rock(theRockType, 500, 500, 100, 100, 100, 0, defaultWeapon, 0, 0);
+                return new Rock(theRockType, 500, 500, 69420, 100, 100, 0, defaultWeapon, 0, 0);
             default:
         }
 
