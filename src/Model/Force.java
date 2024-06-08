b21b542ff06f83bcc7267359a71b08878b673690
zabauka <stasia.loban@hotmail.com>
2024-06-07 21:57:22 -0700

    force doc

--- src/Model/Force.java
+++ src/Model/Force.java
@@ -9,16 +9,54 @@ import java.io.Serializable;
  */
 public class Force implements Serializable {
     private static final long serialVersionUID = 7L;
+
+    /**
+     * The strength of the force.
+     */
     private double myStrength;
+
+    /**
+     * The angle at which the force is applied.
+     */
     private Angle myAngle;
+
+    /**
+     * The decay rate of the force's strength.
+     */
     private double myDecayRate;
 
-    public Force(Angle theAngle, double theStrength, double theDecayRate) {
+    /**
+     * Constructs a Force with the specified angle, strength, and decay rate.
+     *
+     * @param theAngle the angle at which the force is applied
+     * @param theStrength the strength of the force
+     * @param theDecayRate the decay rate of the force's strength
+     */
+    public Force(final Angle theAngle, final double theStrength, final double theDecayRate) {
         setAngle(theAngle);
         setStrength(theStrength);
         setDecayRate(theDecayRate);
     }
 
+    /**
+     * Constructs a Force with the specified angle and strength.
+     * The decay rate is set to 1, meaning the force is applied instantaneously.
+     *
+     * @param theAngle the angle at which the force is applied
+     * @param theStrength the strength of the force
+     */
+    public Force(final Angle theAngle, final double theStrength) {
+        myAngle = theAngle;
+        myStrength = theStrength;
+        myDecayRate = 1;
+    }
+
+    /**
+     * Sets the strength of the force.
+     *
+     * @param theStrength the strength to set
+     * @throws IllegalArgumentException if the strength is negative
+     */
     public void setStrength(final double theStrength) {
         if (theStrength < 0) {
             throw new IllegalArgumentException("Strength cannot be negative");
@@ -26,6 +64,12 @@ public class Force implements Serializable {
         myStrength = theStrength;
     }
 
+    /**
+     * Sets the angle of the force.
+     *
+     * @param theAngle the angle to set
+     * @throws IllegalArgumentException if the angle is null
+     */
     public void setAngle(final Angle theAngle) {
         if (theAngle == null) {
             throw new IllegalArgumentException("Angle cannot be null");
@@ -33,6 +77,12 @@ public class Force implements Serializable {
         myAngle = theAngle;
     }
 
+    /**
+     * Sets the decay rate of the force's strength.
+     *
+     * @param theDecayRate the decay rate to set
+     * @throws IllegalArgumentException if the decay rate is not between 0 and 1
+     */
     public void setDecayRate(final double theDecayRate) {
         if (theDecayRate < 0 || theDecayRate > 1) {
             throw new IllegalArgumentException("Decay rate must be between 0 and 1");
@@ -40,35 +90,56 @@ public class Force implements Serializable {
         myDecayRate = theDecayRate;
     }
 
-    public Force(final Angle theAngle, final double theStrength) {
-        myAngle = theAngle;
-        myStrength = theStrength;
-        myDecayRate = 1;
-    }
-
+    /**
+     * Gets the strength of the force.
+     *
+     * @return the strength of the force
+     */
     public double getStrength() {
         return myStrength;
     }
 
-    // horizontal component of the force
+    /**
+     * Gets the horizontal component of the force.
+     *
+     * @return the horizontal component of the force
+     */
     public double getXStrength() {
         return myStrength * Math.cos(myAngle.getRadians());
     }
 
-    // vertical component of the force
+    /**
+     * Gets the vertical component of the force.
+     *
+     * @return the vertical component of the force
+     */
     public double getYStrength() {
         return myStrength * Math.sin(myAngle.getRadians());
     }
 
+    /**
+     * Gets the angle of the force.
+     *
+     * @return the angle of the force
+     */
     public Angle getAngle() {
         return myAngle;
     }
 
+    /**
+     * Gets the decay rate of the force's strength.
+     *
+     * @return the decay rate of the force's strength
+     */
     public double getDecayRate() {
         return myDecayRate;
     }
 
-    // update the strength of the force based on the decay rate
+    /**
+     * Updates the strength of the force based on the decay rate.
+     *
+     * @return true if the force still has significant strength left, false otherwise
+     */
     public boolean update() {
         myStrength *= (1 - myDecayRate);
         return myStrength > 0.001;  // there's still significant strength left
